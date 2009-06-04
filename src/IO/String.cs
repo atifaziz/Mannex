aa94f460e5cf27a87f044ca60a00f25c7914f94c
Atif Aziz <code@raboof.com>
2009-06-04 16:42:54 +0200

    Added file/path name sanitization methods for String

---
+++ src/IO/String.cs
@@ -0,0 +1,135 @@
+#region License, Terms and Author(s)
+//
+// Mannex - Extension methods for .NET
+// Copyright (c) 2009 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Mannex.IO
+{
+    #region Imports
+
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    #endregion
+
+    /// <summary>
+    /// Extension methods for <see cref="string"/>.
+    /// </summary>
+
+    static partial class StringExtensions
+    {
+        private static readonly char[] _badFileNameChars;
+        private static readonly string _badFileNameCharsPattern;
+        private static readonly char[] _badPathChars;
+        private static readonly string _badPathCharsPattern;
+
+        static StringExtensions()
+        {
+            _badFileNameChars = Path.GetInvalidFileNameChars();
+            _badFileNameCharsPattern = Patternize(_badFileNameChars);
+            _badPathChars = Path.GetInvalidPathChars();
+            _badPathCharsPattern = Patternize(_badPathChars);
+        }
+
+        private static string Patternize(IEnumerable<char> chars)
+        {
+            Debug.Assert(chars != null);
+            return "[" 
+                 + string.Join(string.Empty, chars.Select(ch => Regex.Escape(ch.ToString())).ToArray())
+                 + "]";
+        }
+
+        /// <summary>
+        /// Makes the content of the string safe for use as a file name
+        /// by replacing all invalid characters, those returned by
+        /// <see cref="Path.GetInvalidFileNameChars"/>, with an underscore.
+        /// </summary>
+
+        public static string ToFileNameSafe(this string str)
+        {
+            return ToFileNameSafe(str, null);
+        }
+
+        /// <summary>
+        /// Makes the content of the string safe for use as a file name
+        /// by replacing all invalid characters, those returned by
+        /// <see cref="Path.GetInvalidFileNameChars"/>, with 
+        /// <paramref name="replacement"/>.
+        /// </summary>
+        /// <remarks>
+        /// The <paramref name="replacement"/> string itself cannot 
+        /// carry any invalid file name characters. If 
+        /// <paramref name="replacement"/> is <c>null</c> or empty
+        /// then it assumes the value of an underscore.
+        /// </remarks>
+
+        public static string ToFileNameSafe(this string str, string replacement)
+        {
+            return SanitizePathComponent(str, 
+                (replacement ?? string.Empty).MaskEmpty("_"), 
+                _badFileNameChars, _badFileNameCharsPattern);
+        }
+
+        /// <summary>
+        /// Makes the content of the string safe for use as a file name
+        /// by replacing all invalid characters, those returned by
+        /// <see cref="Path.GetInvalidPathChars"/>, with an underscore.
+        /// </summary>
+
+        public static string ToPathNameSafe(this string str)
+        {
+            return ToPathNameSafe(str, null);
+        }
+
+        /// <summary>
+        /// Makes the content of the string safe for use as a file name
+        /// by replacing all invalid characters, those returned by
+        /// <see cref="Path.GetInvalidPathChars"/>, with 
+        /// <paramref name="replacement"/>.
+        /// </summary>
+        /// <remarks>
+        /// The <paramref name="replacement"/> string itself cannot 
+        /// carry any invalid file name characters. If 
+        /// <paramref name="replacement"/> is <c>null</c> or empty
+        /// then it assumes the value of an underscore.
+        /// </remarks>
+
+        public static string ToPathNameSafe(this string str, string replacement)
+        {
+            return SanitizePathComponent(str,
+                (replacement ?? string.Empty).MaskEmpty("_"),
+                _badPathChars, _badPathCharsPattern);
+        }
+
+        private static string SanitizePathComponent(string str, string replacement, char[] badChars, string badPattern)
+        {
+            Debug.Assert(replacement != null);
+            if (str == null) throw new ArgumentNullException("str");
+            if (str.Length == 0) throw new ArgumentException(null, "str");
+            if (replacement.IndexOfAny(badChars) >= 0) throw new ArgumentException(null, "replacement");
+            return Regex.Replace(str, badPattern, replacement);
+        }
+    }
+}
---
+++ tests/IO/StringTests.cs
@@ -0,0 +1,141 @@
+#region License, Terms and Author(s)
+//
+// Mannex - Extension methods for .NET
+// Copyright (c) 2009 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Mannex.Tests.IO
+{
+    #region Improts
+
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Mannex.IO;
+    using Xunit;
+    using StringExtensions = Mannex.IO.StringExtensions;
+
+    #endregion
+
+    public class StringTests
+    {
+        [Fact]
+        public void ToFileNameSafeFailsWithNullThis()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => StringExtensions.ToFileNameSafe(null));
+            Assert.Equal("str", e.ParamName);
+            
+            e = Assert.Throws<ArgumentNullException>(() => StringExtensions.ToFileNameSafe(null, string.Empty));
+            Assert.Equal("str", e.ParamName);
+        }
+
+        [Fact]
+        public void ToFileNameSafeFailsWithEmptyThis()
+        {
+            var e = Assert.Throws<ArgumentException>(() => string.Empty.ToFileNameSafe());
+            Assert.Equal("str", e.ParamName);
+
+            e = Assert.Throws<ArgumentException>(() => string.Empty.ToFileNameSafe(string.Empty));
+            Assert.Equal("str", e.ParamName);
+        }
+
+        [Fact]
+        public void ToFileNameSafeReplacesInvalidFileNameChars()
+        {
+            Assert.Equal("foo_bar", "foo*bar".ToFileNameSafe());
+            Assert.Equal("foo_bar", "foo*bar".ToFileNameSafe(null));
+        }
+
+        [Fact]
+        public void ToFileNameSafeUsesUnderscoreAsDefaultReplacement()
+        {
+            Assert.Equal("foo-bar", "foo*bar".ToFileNameSafe("-"));
+        }
+
+        [Fact]
+        public void ToFileNameSafeFailsWithReplacementCarryingInvalidFileNameChars()
+        {
+            foreach (var e in Path.GetInvalidFileNameChars()
+                                  .Select(ch => Assert.Throws<ArgumentException>(()
+                                             => "foo".ToFileNameSafe(ch.ToString()))))
+            {
+                Assert.Equal("replacement", e.ParamName);
+            }
+        }
+
+        [Fact]
+        public void ToFileNameSafeReplacesAllInvalidFileNameChars()
+        {
+            var chars = Path.GetInvalidFileNameChars();
+            Assert.Equal(new string('_', chars.Length), new string(chars).ToFileNameSafe());
+        }
+
+        [Fact]
+        public void ToPathNameSafeFailsWithNullThis()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => StringExtensions.ToPathNameSafe(null));
+            Assert.Equal("str", e.ParamName);
+
+            e = Assert.Throws<ArgumentNullException>(() => StringExtensions.ToPathNameSafe(null, string.Empty));
+            Assert.Equal("str", e.ParamName);
+        }
+
+        [Fact]
+        public void ToPathNameSafeFailsWithEmptyThis()
+        {
+            var e = Assert.Throws<ArgumentException>(() => string.Empty.ToPathNameSafe());
+            Assert.Equal("str", e.ParamName);
+
+            e = Assert.Throws<ArgumentException>(() => string.Empty.ToPathNameSafe(string.Empty));
+            Assert.Equal("str", e.ParamName);
+        }
+
+        [Fact]
+        public void ToPathNameSafeReplacesInvalidPathNameChars()
+        {
+            Assert.Equal("foo_bar", "foo|bar".ToPathNameSafe());
+            Assert.Equal("foo_bar", "foo|bar".ToPathNameSafe(null));
+        }
+
+        [Fact]
+        public void ToPathNameSafeUsesUnderscoreAsDefaultReplacement()
+        {
+            Assert.Equal("foo-bar", "foo|bar".ToPathNameSafe("-"));
+        }
+
+        [Fact]
+        public void ToPathNameSafeFailsWithReplacementCarryingInvalidPathNameChars()
+        {
+            foreach (var e in Path.GetInvalidPathChars()
+                                  .Select(ch => Assert.Throws<ArgumentException>(()
+                                             => "foo".ToPathNameSafe(ch.ToString()))))
+            {
+                Assert.Equal("replacement", e.ParamName);
+            }
+        }
+
+        [Fact]
+        public void ToPathNameSafeReplacesAllInvalidPathNameChars()
+        {
+            var chars = Path.GetInvalidPathChars();
+            Assert.Equal(new string('_', chars.Length), new string(chars).ToPathNameSafe());
+        }
+    }
+}
