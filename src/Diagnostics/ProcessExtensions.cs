7a654a9cd48e7d87563fcb6b14e2dcd3d493beb7
Atif Aziz <code@raboof.com>
2009-10-08 19:51:07 +0200

    Added TryKill as an extension method for Process

---
+++ src/Diagnostics/ProcessExtensions.cs
@@ -0,0 +1,72 @@
+#region License, Terms and Author(s)
+//
+// Mannex - Extension methods for .NET
+// Copyright (c) 2009 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Mannex.Diagnostics
+{
+    #region Imports
+
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+
+    #endregion
+
+    /// <summary>
+    /// Extension methods for <see cref="Process"/>.
+    /// </summary>
+
+    static partial class ProcessExtensions
+    {
+        /// <summary>
+        /// Attempts to kill the process identified by the <see cref="Process"/>
+        /// object and returns <c>null</c> on success otherwise the error
+        /// that occurred in the attempt.
+        /// </summary>
+
+        public static Exception TryKill(this Process process)
+        {
+            if (process == null) throw new ArgumentNullException("process");
+
+            try
+            {
+                process.Kill();
+                return null;
+            }
+            catch (InvalidOperationException e)
+            {
+                // Occurs when:
+                // - process has already exited. 
+                // - no process is associated with this Process object.
+                return e;
+            }
+            catch (Win32Exception e)
+            {
+                // Occurs when:
+                // - associated process could not be terminated. 
+                // - process is terminating.
+                // - associated process is a Win16 executable.
+                return e;
+            }
+        }
+    }
+}
