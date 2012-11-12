11e3d93001d6a95849a56738947ecbab7c5b1ab1
azizatif <azizatif@localhost>
2012-11-12 08:30:49 +0100

    Bumping version to 1.2.15412.725

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 [assembly: AssemblyCopyright("Copyright (c) 2009, Atif Aziz. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.14015.0")]
-[assembly: AssemblyFileVersion("1.2.14015.0")]
+[assembly: AssemblyVersion("1.2.15412.0")]
+[assembly: AssemblyFileVersion("1.2.15412.725")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
