a94b1de8833b3fd684d46f15e9576a10598449b2
Atif Aziz <code@raboof.com>
2012-12-07 16:17:44 +0100

    ToInvariantString for IFormattable objects

---
+++ src/IFormattable.cs
@@ -0,0 +1,58 @@
+#region License, Terms and Author(s)
+//
+// Mannex - Extension methods for .NET
+// Copyright (c) 2009 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Mannex
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+    /// <summary>
+    /// Extension methods for <see cref="IFormattable"/>.
+    /// </summary>
+
+    static partial class IFormattableExtensions
+    {
+        /// <summary>
+        /// Formats the value of this object using the invariant culture.
+        /// </summary>
+
+        [DebuggerStepThrough]
+        public static string ToInvariantString(this IFormattable formattable)
+        {
+            return formattable.ToInvariantString(null);
+        }
+
+        /// <summary>
+        /// Formats the value of this object using a specific format and the 
+        /// invariant culture.
+        /// </summary>
+
+        [DebuggerStepThrough]
+        public static string ToInvariantString(this IFormattable formattable, string format)
+        {
+            if (formattable == null) throw new ArgumentNullException("formattable");
+            return formattable.ToString(format, CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ tests/IFormattableTests.cs
@@ -0,0 +1,67 @@
+#region License, Terms and Author(s)
+//
+// Mannex - Extension methods for .NET
+// Copyright (c) 2009 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System;
+
+namespace Mannex.Tests
+{
+    #region Improts
+
+    using System.Globalization;
+    using System.Threading;
+    using Xunit;
+
+    #endregion
+
+    public class IFormattableTests
+    {
+        [Fact]
+        public void ToInvariantString()
+        {
+            IFormattable num = 1234;
+            Assert.Equal("1234", WhileInFrance(() => num.ToInvariantString()));
+        }
+ 
+        [Fact]
+        public void ToInvariantStringWithSpecificFormat()
+        {
+            IFormattable num = 1234;
+            Assert.Equal("1,234.00", WhileInFrance(() => num.ToInvariantString("N2")));
+        }
+
+        static T WhileInFrance<T>(Func<T> function)
+        {
+            var saved = Thread.CurrentThread.CurrentCulture;
+            var thisThread = Thread.CurrentThread;
+            thisThread.CurrentCulture = new CultureInfo("fr-FR");
+            try
+            {
+                return function();
+            }
+            finally
+            {
+                thisThread.CurrentCulture = saved;
+            }
+        }
+    }
+}
