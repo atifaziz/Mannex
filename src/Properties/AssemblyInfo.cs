b4c2fa824cffdc764e6eba8caa300739a414ba1d
azizatif <azizatif@localhost>
2013-06-10 08:34:56 +0200

    Version 2.6

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 [assembly: AssemblyCopyright("Copyright (c) 2009, Atif Aziz. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.5.16106.0")]
-[assembly: AssemblyFileVersion("2.5.16106.1932")]
+[assembly: AssemblyVersion("2.6.16110.0")]
+[assembly: AssemblyFileVersion("2.6.16110.633")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
