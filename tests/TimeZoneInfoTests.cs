b43bdaf0b021f31a65da7784747b1d445c2c9d83
Philippe Raemy <praemy61@gmail.com>
2015-05-26 10:25:22 +0200

    =formatting

--- tests/TimeZoneInfoTests.cs
+++ tests/TimeZoneInfoTests.cs
@@ -36,7 +36,7 @@ namespace Mannex.Tests
                         new DateTime(1900, 1, 1), new DateTime(2100, 1, 1), new TimeSpan(1, 30, 0),
                         TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(1, 1, 1, 2, 0, 0), 2, 15),
                         TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(1, 1, 1, 2, 0, 0), 2, 20)
-                                                                                                                                                             )
+                    )
                 });
             Assert.Equal(24.0, tz.HoursInDay(new DateTime(2015, 1, 1)));
             Assert.Equal(22.5, tz.HoursInDay(new DateTime(2015, 2, 15)));
