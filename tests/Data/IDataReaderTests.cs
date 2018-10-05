20ba5ae4a7fd67083f69f95ae03b2f9dd1c695a5
Atif Aziz <code@raboof.com>
2018-10-05 10:41:38 +0200

    Remove commented-out tests

--- tests/Data/IDataReaderTests.cs
+++ tests/Data/IDataReaderTests.cs
@@ -458,24 +458,5 @@ namespace Mannex.Tests.Data
 
             #endregion
         }
-        /*
-                [Fact]
-                public void FindReturnsDefaultWhenKeyKeyNotPresent()
-                {
-                    Assert.Equal(0, new Dictionary<int, int>().Find(42));
-                }
-
-                [Fact]
-                public void FindReturnsSpecificDefaultWhenKeyKeyNotPresent()
-                {
-                    Assert.Equal(-42, new Dictionary<int, int>().Find(42, -42));
-                }
-
-                [Fact]
-                public void FindReturnsValueOfPresentKey()
-                {
-                    var dict = new Dictionary<int, string> { { 42, "fourty two" } };
-                    Assert.Equal("fourty two", dict.Find(42));
-                }*/
     }
 }
